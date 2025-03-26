42739f821298bbe597c04f10141ac3d534d3ede6
211089 <211089@trinityschools.nz>
2025-03-27 08:36:12 +1300

    New

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require ('http')
 const fs = require('fs')
-const port = 3000
+const app = express();
+const port = 5000
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -25,3 +26,39 @@ server.listen(port, function(error){
         console.log('Server is listening on port ' + port)
     }
 })
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const DATA_FILE = path.join(__dirname, "data", "flashcards.json");
+
+const loadFlashcards = () => {
+    try {
+        const data = fs.readFileSync(DATA_FILE);
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+};
+
+const saveFlashcards = (flashcards) => {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(flashcards, null, 2));
+};
+
+app.get("/", (req, res) => {
+    const flashcards = loadFlashcards();
+    res.render("index", { flashcards });
+});
+
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+app.post("/add", (req, res) => {
+    const { question, answer } = req.body;
+    let flashcards = loadFlashcards();
+    flashcards.push({ question, answer });
+    saveFlashcards(flashcards);
+    res.redirect("/");
+});
